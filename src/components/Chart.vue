bfe61c245118548d96b1b3845a03d1e5967e0aab
AkashiSai <akashi_sai@163.com>
2019-12-18 11:59:45 +0800

    added v-chart component

---
+++ src/components/Chart.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="chart" :option="option"></div>
+</template>
+
+<script>
+    import HighCharts from 'highcharts'
+    export default {
+        name: "Chart",
+        props: {
+            option: {
+                type: Object
+            }
+        },
+        watch: {
+            option() {
+                HighCharts.chart(this.$el, this.option);
+            }
+        },
+        mounted () {
+            HighCharts.chart(this.$el, this.option)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
