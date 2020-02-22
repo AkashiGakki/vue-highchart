e8f73ee874008b2d4fdf7849ccd75bcf676711b3
akashi_sai <akashi_sai@163.com>
2020-02-22 16:11:31 +0800

    added theme

---
+++ v2/src/themes/avocado.js
@@ -0,0 +1,23 @@
+export default {
+    colors: ["#F3E796", "#95C471", "#35729E", "#251735"],
+    colorAxis: {
+        maxColor: "#05426E",
+        minColor:
+            "#F3E796"
+    }
+    ,
+    plotOptions: {
+        map: {
+            nullColor: "#FCFEFE"
+        }
+    }
+    ,
+    navigator: {
+        maskFill: "rgba(170, 205, 170, 0.5)",
+        series: {
+            color: "#95C471",
+            lineColor:
+                "#35729E"
+        }
+    }
+}
---
+++ v2/src/themes/olive-green.js
@@ -0,0 +1,21 @@
+export default {
+    chart: {
+        backgroundColor: '#272822'
+    },
+    title: {
+        style: {
+            color: 'white'
+        }
+    },
+    legend: {
+        title: {
+            style: {
+                color: 'silver'
+            }
+        }
+    },
+    colorAxis: {
+        minColor: '#373832',
+        maxColor: '#baFF55'
+    }
+}
